f7e9fb41af49c3b91d91e6f3e903006173493d0a
JangGeunSong <sjg1468@gmail.com>
2020-02-11 21:25:50 +0900

    클릭 테스트 코드 완성

--- src/__test__/memosheet.test.tsx
+++ src/__test__/memosheet.test.tsx
@@ -13,9 +13,18 @@ describe('Memosheet test', () => {
     it('send to modal render test', async () => {
         const memosheet = render(<MemoSheet />)
         await wait(() =>  {
-            getByText(memosheet.container, 'title1')
+            fireEvent.click(getByText(memosheet.container, 'title1'))
         })
         expect(memosheet.asFragment()).toMatchSnapshot()
-        // 추가적인 방안 모색 필요
+        // 클릭 이벤트의 경우 fireEvent 를 불러와서 활용하면 된다. 이때 useEffect를 사용한 함수형 컴포넌트의 경우 훅스 자체가 async함수로 되어 있으므로 반드시 await wait()를 호출하여 데이터가 API를 통해 fetch 되는것을 체크하는것이 가장 중요하다.
+    })
+
+    it('modal cancel test', async () => {
+        const memosheet = render(<MemoSheet />)
+        await wait(() => {
+            fireEvent.click(getByText(memosheet.container, 'title1'))
+            fireEvent.click(getByText(memosheet.container, 'Close'))
+        })
+        expect(memosheet.container.firstChild).toHaveClass('memo-sheet')
     })
 })
